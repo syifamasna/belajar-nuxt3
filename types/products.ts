2d1dda3cc63e85d06eee2151304450c68ac57964
Syifa Khairunisa Masna <yhappyvirus@gmail.com>
2023-11-16 14:23:56 +0700

    error semua hiks

--- types/products.ts
+++ types/products.ts
@@ -2,10 +2,21 @@ export interface Products {
     id?: number,
     name?: string,
     category?: string,
-    price?: string,
+    price?: number,
     image?: string,
     color?: string,
     isCart?: boolean,
     description?: string,
     quantity?: number,
     }
+
+export interface FormField {
+    id?: string;
+    name?: string;
+    label?: string;
+    type?: string;
+    value?: string | object;
+    placeholder?: string;
+    checked?: boolean;
+    required?: boolean;
+}
